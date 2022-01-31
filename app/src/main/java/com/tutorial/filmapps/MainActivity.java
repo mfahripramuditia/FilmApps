d16a3109df82668ee2d34b4ab1d462d209da989e
mfahripramuditia <mfahripramuditia@gmail.com>
2022-01-31 20:17:55 +0700

    mengubah isi list

--- app/src/main/java/com/tutorial/filmapps/MainActivity.java
+++ app/src/main/java/com/tutorial/filmapps/MainActivity.java
@@ -41,35 +41,9 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(filmAdapter);
 
-        filmModelList.add(new FilmModel("ETERNALS","MARVEL STUDIOS",
-                getApplicationContext().getString(R.string.deskripsi1), R.drawable.eternals));
+        filmModelList.add(new FilmModel("KEP. KARIMUNJAWA","KAB. JEPARA, JAWA TENGAH",
+                getApplicationContext().getString(R.string.deskripsi1), R.drawable.karimunjawa));
 
-        filmModelList.add(new FilmModel("JUMANJI","20th CENTURY FOX ",
-                getApplicationContext().getString(R.string.deskripsi2),R.drawable.jumanji));
-
-        filmModelList.add(new FilmModel("DEADPOOL","20th CENTURY FOX ",
-                getApplicationContext().getString(R.string.deskripsi3),R.drawable.deadpool1));
-
-        filmModelList.add(new FilmModel("MORTAL KOMBAT","NEW LINE CINEMA ",
-                getApplicationContext().getString(R.string.deskripsi4),R.drawable.mortalkombat));
-
-        filmModelList.add(new FilmModel("SPIDERMAN : NO WAY HOME","MARVEL STUDIOS, COLOUMBIA PICTURES ",
-                getApplicationContext().getString(R.string.deskripsi5),R.drawable.spiderman));
-
-        filmModelList.add(new FilmModel("EXTRACTION"," AGBO, Thematic Entertainment",
-                getApplicationContext().getString(R.string.deskripsi6),R.drawable.extraction));
-
-        filmModelList.add(new FilmModel("PACIFIC RIM : UPRISING","LEGENDARY PICTURES",
-                getApplicationContext().getString(R.string.deskripsi7),R.drawable.pacificrim));
-
-        filmModelList.add(new FilmModel("REAL STEEL","DREAMWORK PICTURES",
-                getApplicationContext().getString(R.string.deskripsi8),R.drawable.realsteel));
-
-        filmModelList.add(new FilmModel("SUICIDE SQUAD","DC ENTERTAIMENT",
-                getApplicationContext().getString(R.string.deskripsi9),R.drawable.suicidesquad));
-
-        filmModelList.add(new FilmModel("WONDER WOMAN","DC FILMS",
-                getApplicationContext().getString(R.string.deskripsi10),R.drawable.wonderwoman));
 
 
         filmAdapter.notifyDataSetChanged();
